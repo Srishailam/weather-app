1d27968d72ec9203bbe9bff929d6e6afad353ccd
Srishailam Dasari <srishailam.dasari@uipath.com>
2021-01-10 19:29:05 -0500

    Issue#6 -fixed - Page does not change when Nav is clicked

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Forecast from "./Forecast";
 import Home from "./Home";
 
 export default () => (
   <main>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/forecast" component={Home} />
+      <Route path="/forecast" component={Forecast} />
     </Switch>
   </main>
 );
